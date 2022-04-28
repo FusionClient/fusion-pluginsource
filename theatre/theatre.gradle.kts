f7b6684c3a37081b3246490cf4f1ef09b722ef54
Fusion Plugins <fusionplugins@gmail.com>
2022-04-28 11:52:10 +1000

    Update / Weaponmap

--- theatre/theatre.gradle.kts
+++ theatre/theatre.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "3.0.7"
+version = "3.0.8"
 
 project.extra["PluginName"] = "[F] Theatre"
 project.extra["PluginDescription"] = "All-in-one plugin for Theatre of Blood"
